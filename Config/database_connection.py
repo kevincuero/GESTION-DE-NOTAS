ec3b7389fd483bc14a375867cb2f978f09a3f63c
Kevi cuero <ig0243204@gmail.com>
2025-11-27 04:30:31 -0500

    DESPLIEGUE DE LA APP EN RENDER #1

--- Config/database_connection.py
+++ Config/database_connection.py
@@ -1,14 +1,19 @@
 import mysql.connector
 from mysql.connector import Error
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 def create_connection():
     """Crea y devuelve una conexión a la base de datos."""
     try:
         connection = mysql.connector.connect(
-            host='localhost',
-            user='root',  
-            password='', 
-            database='GestionDeEstudiantes'  
+            host=os.getenv('DB_HOST', 'localhost'),
+            user=os.getenv('DB_USER', 'root'),  
+            password=os.getenv('DB_PASSWORD', ''), 
+            database=os.getenv('DB_NAME', 'GestionDeEstudiantes'),
+            port=int(os.getenv('DB_PORT', '3306'))
         )
         return connection
     except Error as e:
